6750756b9e2a7564b3f4c33597522e8c095d3bad
Pablo Alpizar <pablo@driveinap.com>
2013-07-20 08:38:11 +0000

    Corrected file name

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync(index.html));
+  response.send(fs.readFileSync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
