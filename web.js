6ca8f3a6e46b4b962378af27c51a2aab420d6251
Ubuntu <ubuntu@ip-172-31-45-138.us-west-2.compute.internal>
2013-07-28 19:40:58 +0000

    Web.js edited to listen in port 8080

--- web.js
+++ web.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var htmlfile = "index.html";
 //var buf = require('buffer');
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   //response.send(buf.toString(fs.readFileSync("index.html")));
-    response.send(fs.readFileSync("index.html","utf8"));
+  //response.send(fs.readFileSync("index.html","utf8"));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
